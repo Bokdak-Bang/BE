fa287b8fc394fcf16ffd3e01feb45f174bf15c71
chahyunsoo <chahs1012@naver.com>
2024-04-30 17:14:30 +0900

    [delete] : 패턴 변경으로 인한 파일 삭제

--- src/main/java/seoul/gonggong/domain/user/service/UserFormatter.java
+++
@@ -1,9 +0,0 @@
-package seoul.gonggong.domain.user.service;
-
-import seoul.gonggong.appllication.user.service.dto.response.UserResponse;
-
-public interface UserFormatter {
-    UserResponse format(Long id);
-
-    UserResponse format(String email);
-}
--- src/main/java/seoul/gonggong/domain/user/service/UserModifier.java
+++
@@ -1,9 +0,0 @@
-package seoul.gonggong.domain.user.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class UserModifier {
-}
