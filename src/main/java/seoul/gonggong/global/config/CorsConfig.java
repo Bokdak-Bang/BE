4ea382e18237e50de08ec48cf38a32b51fc2f01d
chahyunsoo <chahs1012@naver.com>
2024-05-03 12:26:41 +0900

    [feat] : CorsConfig 설정 추가

---
+++ src/main/java/seoul/gonggong/global/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package seoul.gonggong.global.config;
+
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
