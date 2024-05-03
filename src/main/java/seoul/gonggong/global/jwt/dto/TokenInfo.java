f0cd9856272fb1ebb1fb86e0e03576d67fd3daeb
chahyunsoo <chahs1012@naver.com>
2024-05-03 11:40:04 +0900

    [feat] : TokenInfo 반환 클래스 추가
    refresh는 일단 지금은 안씀

---
+++ src/main/java/seoul/gonggong/global/jwt/dto/TokenInfo.java
@@ -0,0 +1,18 @@
+package seoul.gonggong.global.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenInfo {
+    private String grantType;
+    private String accessToken;
+//    private String refreshToken;
+    private Long accessTokenExpiresIn;
+}
+
