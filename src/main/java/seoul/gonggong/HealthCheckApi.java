347e72e086ce912ae3229c5b1b010b32c66c2f41
chahyunsoo <chahs1012@naver.com>
2024-05-09 04:15:42 +0900

    [feat] : 지역 저장 api 구현

--- src/main/java/seoul/gonggong/HealthCheckApi.java
+++ src/main/java/seoul/gonggong/HealthCheckApi.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-
 public class HealthCheckApi {
     @GetMapping("/")
     public ResponseEntity<HttpStatus> hello() {
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping("/health-check")
+    public String check() {
+        return "hello";
     }
 
 }
