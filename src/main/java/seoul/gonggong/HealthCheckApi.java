13ba3504ae2e59ded679615762e07405314aab0d
chahyunsoo <chahs1012@naver.com>
2024-05-09 01:02:46 +0900

    [fix] : Healthcheck 컨트롤러 수정

--- src/main/java/seoul/gonggong/HealthCheckApi.java
+++ src/main/java/seoul/gonggong/HealthCheckApi.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 
 public class HealthCheckApi {
-    @GetMapping("/health-check")
+    @GetMapping("/")
     public String hello() {
         return "Hello!";
     }
