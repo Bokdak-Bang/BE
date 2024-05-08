463bec553b1d7fd063087f379a3dadd15a54c0f2
chahyunsoo <chahs1012@naver.com>
2024-05-08 22:58:12 +0900

    [feat] : PopulationJpaRepository 메소드 추가

--- src/main/java/seoul/gonggong/domain/data/repository/PopulationJpaRepository.java
+++ src/main/java/seoul/gonggong/domain/data/repository/PopulationJpaRepository.java
@@ -2,8 +2,13 @@ package seoul.gonggong.domain.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import seoul.gonggong.domain.data.domain.Area;
 import seoul.gonggong.domain.data.domain.Population;
+import seoul.gonggong.domain.data.domain.WelfareCulture;
+
+import java.util.Optional;
 
 @Repository
 public interface PopulationJpaRepository extends JpaRepository<Population, Long> {
+    Optional<Population> findByArea(Area area);
 }
